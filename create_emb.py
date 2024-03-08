3f0e438b0cf0781262a000c520477685daad2bc5
srinivasc <srinivasc@zenoti.com>
2024-03-08 19:54:20 +0530

    sql examples

--- create_emb.py
+++ create_emb.py
@@ -4,20 +4,16 @@ from sentence_transformers import SentenceTransformer
 
 # Let's make a semantic search for Sci-Fi books! 
 documents = [
-  { "name": "The Time Machine", "description": "A man travels through time and witnesses the evolution of humanity.", "author": "H.G. Wells", "year": 1895 },
-  { "name": "Ender's Game", "description": "A young boy is trained to become a military leader in a war against an alien race.", "author": "Orson Scott Card", "year": 1985 },
-  { "name": "Brave New World", "description": "A dystopian society where people are genetically engineered and conditioned to conform to a strict social hierarchy.", "author": "Aldous Huxley", "year": 1932 },
-  { "name": "The Hitchhiker's Guide to the Galaxy", "description": "A comedic science fiction series following the misadventures of an unwitting human and his alien friend.", "author": "Douglas Adams", "year": 1979 },
-  { "name": "Dune", "description": "A desert planet is the site of political intrigue and power struggles.", "author": "Frank Herbert", "year": 1965 },
-  { "name": "Foundation", "description": "A mathematician develops a science to predict the future of humanity and works to save civilization from collapse.", "author": "Isaac Asimov", "year": 1951 },
-  { "name": "Snow Crash", "description": "A futuristic world where the internet has evolved into a virtual reality metaverse.", "author": "Neal Stephenson", "year": 1992 },
-  { "name": "Neuromancer", "description": "A hacker is hired to pull off a near-impossible hack and gets pulled into a web of intrigue.", "author": "William Gibson", "year": 1984 },
-  { "name": "The War of the Worlds", "description": "A Martian invasion of Earth throws humanity into chaos.", "author": "H.G. Wells", "year": 1898 },
-  { "name": "The Hunger Games", "description": "A dystopian society where teenagers are forced to fight to the death in a televised spectacle.", "author": "Suzanne Collins", "year": 2008 },
-  { "name": "The Andromeda Strain", "description": "A deadly virus from outer space threatens to wipe out humanity.", "author": "Michael Crichton", "year": 1969 },
-  { "name": "The Left Hand of Darkness", "description": "A human ambassador is sent to a planet where the inhabitants are genderless and can change gender at will.", "author": "Ursula K. Le Guin", "year": 1969 },
-  { "name": "The Time Traveler's Wife", "description": "A love story between a man who involuntarily time travels and the woman he loves.", "author": "Audrey Niffenegger", "year": 2003 }
-]
+    {
+        "Question": "What is my last 6 months sales",
+        "SQLQuery": """ /*As the number of days(6*30=180) is greater than 45 days showing monthly sales*/SELECT MONTH(sale_date) As 'Month', SUM(sale_price) AS 'Sum of Monthly Sales' FROM Sales_AI  WHERE sale_date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 6, 0)
+  AND sale_date <  DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)   GROUP BY MONTH(sale_date)""",
+    },
+    {
+        "Question": "list my invoice sales for the last month",
+        "SQLQuery": """ /*As the number of days(30) is less than 45 days showing daily sales*/ SELECT FORMAT(sale_date,'MM-dd-yy') As 'Day Of Month', SUM(sale_price) AS 'Sum of Daily Sales' FROM Sales_AI  WHERE sale_date >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 1, 0)
+  AND sale_date < DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)  GROUP BY sale_date ORDER BY sale_date"""
+    }]
 
 def handler(event,context):
     collections = event["queryStringParameters"]["collection"]
